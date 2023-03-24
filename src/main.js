e0b6cade865daac457cd3b546d0f2da8187a2aa5
Nasser <nasser.kessas@outlook.com>
2023-03-24 17:26:21 +1000

    Removed .env

--- src/main.js
+++ src/main.js
@@ -157,7 +157,7 @@ document.forms.contact_form.onsubmit = function () {
         message: $("textarea[name=message]").val()
     }
 
-    emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, templateParams, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+    emailjs.send("service_58c03od", "template_oyzga7q", templateParams, "S29-L0j1wrSz6Q8vz")
 	    .then((response) => {
 	        console.log('SUCCESS!', response.status, response.text);
 	    }, (err) => {
